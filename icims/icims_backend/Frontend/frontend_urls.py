413a676ece942510cc41c9c10f74e25aa5c0d6b8
Arman <paplix.dev@gmail.com>
2025-09-11 22:06:43 +0800

    Frontend Client Adding

---
+++ icims/icims_backend/Frontend/frontend_urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from .Client import ClientAddFrontEnd
+
+urlpatterns = [
+
+    # Client
+    path('client/input/', ClientAddFrontEnd.as_view(), name='client_input'),
+
+]
